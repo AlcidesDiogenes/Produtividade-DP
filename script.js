09d66c2dda2df2006dabc795480a1ede3804e9fa
AlcidesDiogenes <alcidesdiogenes@gmail.com>
2025-07-09 21:36:24 -0300

    Implementação de melhoria

--- script.js
+++ script.js
@@ -1,86 +1,22 @@
-import { openLink, creatbtn, selectById, limparBotoes} from './utils.js';
-
-const btnCertidoes = selectById('certidoes');
-const btnSindicatos = selectById('sindicatos');
-const btnCalculos = selectById('calculos');
-const btnAtv1 = selectById('btnAtv1');
-
-let grupoAtivo = null;
-let css = null;
-
-function mostrarCertidoes() {
-  limparBotoes(btnAtv1);
-  grupoAtivo = 'certidoes';
-  
-  const btnCPF = creatbtn('btnCPF', 'CPF');
-  const btnCNPJ = creatbtn('btnCNPJ', 'CNPJ');
-  const btnsimples = creatbtn('btnsimples', 'Simples Nacional');
-
-  openLink(btnCPF, 'https://servicos.receita.fazenda.gov.br/servicos/cpf/consultasituacao/consultapublica.asp');
-  openLink(btnCNPJ, 'https://solucoes.receita.fazenda.gov.br/servicos/cnpjreva/cnpjreva_solicitacao.asp');
-  openLink(btnsimples, 'https://www8.receita.fazenda.gov.br/simplesnacional/aplicacoes.aspx?id=21');
-
-  btnAtv1.appendChild(btnCPF);
-  btnAtv1.appendChild(btnCNPJ);
-  btnAtv1.appendChild(btnsimples);
-  }
 
 
-function mostrarSindicatos() {
-  limparBotoes(btnAtv1);
-  grupoAtivo = 'sindicatos';
-  
+  const botaocetidoes = document.getElementById("botao-certidoes");
+  const listacertidoes = document.getElementById("lista-certidoes");
 
-    const btncomerciof = creatbtn('btncomerciof', 'Comércio Fortaleza');
-    const btnsindicam = creatbtn('btnsindicam', 'SINDICAM Ceará');
+  const botaogoverno = document.getElementById("botao-governo");
+  const listagoverno = document.getElementById("lista-governamentais");
 
-    openLink(btncomerciof, 'https://sindcomerciarios.org.br/');
-    openLink(btnsindicam, 'https://sindicamceara.org.br/');
+  const botaoCalculos = document.getElementById("calculos");
+  const listacalculos = document.getElementById("lista-calculos");
 
-    btnAtv1.appendChild(btncomerciof);
-    btnAtv1.appendChild(btnsindicam);
-  }
+ botaocetidoes.addEventListener("click", () => {
+    listacertidoes.classList.toggle("escondido");
+  });
 
-function mostrarCalculos() {
-  limparBotoes(btnAtv1);
-  grupoAtivo = 'calculos';
+  botaogoverno.addEventListener("click", () => {
+    listagoverno.classList.toggle("escondido");
+  });
 
-  const btnFGTS = creatbtn('btnFGTS', 'FGTS');
-  const btnINSS = creatbtn('btnINSS', 'INSS');
-  const btnIRPF = creatbtn('btnIRPF', 'IRPF');
-
-  openLink(btnFGTS, 'https://www.fgts.gov.br/fgts/calculo-fgts/');
-  openLink(btnINSS, 'https://www.gov.br/pt-br/servicos/calcular-contribuicao-ao-inss');
-  openLink(btnIRPF, 'https://www.gov.br/receitafederal/pt-br/assuntos/irpf');
-
-  btnAtv1.appendChild(btnFGTS);
-  btnAtv1.appendChild(btnINSS);
-  btnAtv1.appendChild(btnIRPF);
-}
-
-btnCertidoes.addEventListener('click', () => {
-  if (grupoAtivo === 'certidoes') {
-    limparBotoes(btnAtv1);
-    grupoAtivo = null;;
-  } else {
-    mostrarCertidoes();
-  }
-});
-
-btnSindicatos.addEventListener('click', () => {
-  if (grupoAtivo === 'sindicatos') {
-    limparBotoes(btnAtv1);
-    grupoAtivo = null;
-  } else {
-    mostrarSindicatos();
-  }
-});
-
-btnCalculos.addEventListener('click', () => {
-  if (grupoAtivo === 'calculos') {
-    limparBotoes(btnAtv1);
-    grupoAtivo = null;
-  } else {
-    mostrarCalculos();
-  }
-});
+  botaoCalculos.addEventListener("click", () => {
+    listacalculos.classList.toggle("escondido");
+  });
--- teste.js
+++
@@ -1,9 +0,0 @@
-function ativarCertidoes() {
-
-}
-
-function openLink(url) {
-  bnt.addEventListener('click', () => {
-    window.open('url', '_blank')
-  })
-}
